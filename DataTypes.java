ee212c347902b6d7e498589b98783ee6f7d96908
Karan Atreya <karan1988@gmail.com>
2023-03-09 07:05:04 +0100

    DataTypes, IfElse, IncrementDecrement, Loops, StringConcat, SwitchCase

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+package JavaSessions;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		//byte b=10;
+		//byte b1=70;
+		//System.out.println(b+b1);
+		
+		long g = 645645645645654654L;
+		System.out.println(g);
+
+	}
+
+}
---
+++ LoopsConcept.java
@@ -0,0 +1,84 @@
+package JavaSessions;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class LoopsConcept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		int i=1;
+//		while(i<=10) {
+//			System.out.println(i);
+//			//i++;
+//			//++i;
+//			i=i+1;
+//		}
+		
+//		int k=1;
+//		while(true) {
+//			System.out.println(k);
+//			k++;
+//			if(k==20) {
+//				break;
+//			}
+//		}
+		
+//		System.out.println("--------------------");
+//		int p=1;
+//		for(;p<=10;) {
+//			//p++;
+//			System.out.println(p);
+//			//p++;
+//			//++p;
+//			p=p+1;
+//		}
+		
+//		for(int g=1;g<=10;) {
+//			System.out.println(g);
+//		}
+		
+//		for(;;) {
+//			System.out.println("welcome to hotel taj");
+//		}
+		
+//		for(int even=2;even<=10;even=even+2) {
+//			System.out.println(even);
+//		}
+
+//		for(double d=1.0;d<=10.0;d++) {
+//			System.out.println(d);
+//		}
+		
+//		for(char c='A'; c<='Z'; c++) {
+//			System.out.println(c);
+//		}
+		
+//		for(int as=97;as<=122;as++) {
+//			System.out.println(as);
+//		}
+		
+//		for(int as=97;as<=122;as++) {
+//			System.out.println((char)as);
+//		}
+		
+//		System.out.println(10%2);
+//		System.out.println(10%3);
+		
+//		for (int t=1;t<=100;t++) {
+//			System.out.println(t);
+//			if(t%5==0) {
+//				System.out.println("hi");
+//			}
+//		}
+		
+		int r=1;
+		do {
+			r++;
+			System.out.println(r);
+			break;
+			
+		} while (r<=10);
+	}
+
+}
